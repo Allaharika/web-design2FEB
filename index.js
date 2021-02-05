f7c1beed467cdf63191e725414797e51c681ad39
Allaharika <78401316+Allaharika@users.noreply.github.com>
2021-02-05 14:37:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-//var va r roo
-//parent element
-var container=document.querySelector("#root")
-//child element
-var card=document.createElement("div");
-card.setAttribute("id","card");
-container.appendChild(card)
+// //var va r roo
+// //parent element
+// var container=document.querySelector("#root")
+// //child element
+// var card=document.createElement("div");
+// card.setAttribute("id","card");
+// container.appendChild(card)
 
 
-var image=document.createElement("img");
-image.src="C:\Users\Administrator\Desktop\web-design2FEB\image"
-image.alt="Image"
-image.style.width="20%"
-card.appendChild(image)
+// var image=document.createElement("img");
+// image.src="C:\Users\Administrator\Desktop\web-design2FEB\image"
+// image.alt="Image"
+// image.style.width="20%"
+// card.appendChild(image)
+
+
+function loadJSON(file,callback){
+    var ajax=new XMLHttpRequest();
+    ajax.overrideMimeType("application/json");
+    ajax.open("GET",file,true);
+    ajax.onreadystatechange=function(){
+        if(ajax.readyState===4 && ajax.status===200){
+            callback(ajax.responseText);
+        }
+        ajax.send();
+    }
+   
+}
+loadJSON("data.json",function(text){
+    var d=JSON.parse(text)
+    console.log(d)
+})
